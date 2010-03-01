e60112d648518bae70e73c913de6a9dead840a7f
Michael Gelbart <mgelbart@princeton.edu>
2010-03-01 16:23:45 -0500

    changed drawCellSmall so that it captures the region a little better. there doesn't seem to be a single right way of doing this, but hopefully this modification is helpful

--- Measurements/drawCellSmall.m
+++ Measurements/drawCellSmall.m
@@ -2,9 +2,10 @@ function [img r] = drawCellSmall(embryo, t, z, c)
 % draws an image of the cell in a bounding box. the image is returned in
 % "img" and the coordinates of the top-left corner are returned in "r"
 
-vcoords = round(embryo.getCellGraph(t, z).getCell(c).vertexCoords);
+vcoords = embryo.getCellGraph(t, z).getCell(c).vertexCoords;
 r = min(vcoords);
 
-vcoords(:, 1) = vcoords(:, 1) - min(vcoords(:, 1));
-vcoords(:, 2) = vcoords(:, 2) - min(vcoords(:, 2));
-img = poly2mask(vcoords(:, 2), vcoords(:, 1), ceil(max(vcoords(:,1))), ceil(max(vcoords(:, 2))));
+vcoords(end+1, :) = vcoords(1, :);
+vcoords(:, 1) = vcoords(:, 1) - min(vcoords(:, 1)) + 0.5;
+vcoords(:, 2) = vcoords(:, 2) - min(vcoords(:, 2)) + 0.5;
+img = poly2mask(vcoords(:, 2), vcoords(:, 1), floor(max(vcoords(:,1))), floor(max(vcoords(:, 2))));
