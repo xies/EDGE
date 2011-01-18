25411b3c410c2139b74c3684d71434b781362431
Michael Gelbart <mgelbart@princeton.edu>
2011-01-18 18:27:03 -0500

    eta

--- Meta Measurements/nucleus_patterning.m
+++ Meta Measurements/nucleus_patterning.m
@@ -1,4 +1,4 @@
-clear all; close all; clc;
+clear all;  clc; %close all;
 
 %% look at the spatial patterning of nucleus drops
 
@@ -16,10 +16,14 @@ layers_from_top = 3;
 %%
 
 
-nuclear_position = NaN(size(data_all,1),size(data_all,3));
-nucleus_drop = NaN(size(data_all, 3), 1);
-for j = 1:size(data_all,3)
-    for i = 1:size(data_all, 1)
+tlength = size(data_all, 1);
+zlength = size(data_all, 2);
+clength = size(data_all, 3);
+
+nuclear_position = NaN(tlength,clength);
+nucleus_drop = NaN(clength, 1);
+for j = 1:clength
+    for i = 1:tlength
         nuclear_position(i,j) = get_nuclear_position(data_all, i, j);
     end
     nucleus_drop(j) = find_nucleus_drop(nuclear_position(:,j));
@@ -27,31 +31,67 @@ end
 
 %% do the correlation thing with dropping times
 norder = 5;
-neigh = extract_neighborhood(data_set, cell_inds, norder);
-
+% neigh = extract_neighborhood(data_set, cell_inds, norder);
+load neighbor_info_090309
 
 %%
 masterlayer = 9;
+mastertime = 1;
+dropping_dt = NaN(length(cell_inds), norder);
+dropping_xcor = NaN(length(cell_inds), norder, 2*tlength-1);
 
-dropping = NaN(length(cell_inds), norder);
+nuclear_position_noNaN = nuclear_position;
+nuclear_position_noNaN(isnan(nuclear_position_noNaN)) = 0;
+
 for i = 1:length(cell_inds)
     droptime = nucleus_drop(i);
     
-    if isnan(droptime)
-        continue;
+    % compute dropping_dt
+    if ~isnan(droptime)
+        for j = 1:norder
+            dropping_dt(i, j) = ...
+                my_mean( abs( droptime - nucleus_drop( neigh{i, droptime, masterlayer, j} ) ).' );
+        end
+    end   
+    
+    % compute xcorr
+    for j = 1:norder
+        cor = zeros(2*tlength-1, 1);
+        for k = 1:length(neigh{i, mastertime, masterlayer, j})
+            
+            newcorr = xcorr(nuclear_position_noNaN(:,i), ...
+                      nuclear_position_noNaN(:,(neigh{i, mastertime, masterlayer, j}(k))), 'coeff');
+            cor = cor + newcorr;
+        end
+        dropping_xcor(i, j, :) = cor / length(neigh{i, mastertime, masterlayer, j});
     end
     
-    for j = 1:norder
-        dropping(i, j) = ...
-            my_mean( abs( droptime - nucleus_drop( neigh{i, droptime, masterlayer, j} ) ).' );
-    end   
 end
 
-dropping_avg = my_mean(dropping.');
-%%
-figure; bar(1:norder, dropping_avg)
+dropping_dt_avg = my_mean(dropping_dt.');
 
-%%
+%% cross corr of nucleus position
+dropping_xcor_avg = NaN(norder, size(dropping_xcor, 3));
+for i = 1:size(dropping_xcor, 3)
+    dropping_xcor_avg(:, i) = my_mean(dropping_xcor(:,:,i)');
+end
+figure; plot(-tlength+1:tlength-1, dropping_xcor_avg');
+legend('1','2','3','4','5');
+xlabel('delay')
+ylabel('normalized cross correlation')
+title('cross correlation of nucleus position over time for different neighbor orders')
+
+%% cross corr at zero lag vs order
+figure; plot(1:norder, dropping_xcor_avg(:, tlength-1), '.');
+xlabel('neighbor order');
+ylabel('cross correlation at zero lag')
+
+%% avg bar graph of delta t vs neighbor order
+figure; bar(1:norder, dropping_dt_avg*19.5/60)
+xlabel('neighbor order')
+ylabel('average absolute value of time difference (minutes');
+
+%% movie of bar graphs with delta t vs neighbor order
 for i = 1:size(dropping, 1);
     if any(isnan(dropping(i,:)))
         continue;
@@ -61,6 +101,46 @@ for i = 1:size(dropping, 1);
     title(i);
     pause(0.3)
 end
+
+%% x-corr with velocity
+%%
+masterlayer = 9;
+mastertime = 1;
+dropping_dt = NaN(length(cell_inds), norder);
+dropping_v_xcor = NaN(length(cell_inds), norder, 2*tlength-1-2);
+
+
+nuclear_v = diff(nuclear_position);
+nuclear_v_noNaN(isnan(nuclear_v)) = 0;
+
+
+for i = 1:length(cell_inds)
+    % compute xcorr
+    for j = 1:norder
+        cor = zeros(2*tlength-1-2, 1);
+        for k = 1:length(neigh{i, mastertime, masterlayer, j})
+            
+            newcorr = xcorr(nuclear_v_noNaN(:,i), ...
+                      nuclear_v_noNaN(:,(neigh{i, mastertime, masterlayer, j}(k))), 'coeff');
+            cor = cor + newcorr;
+        end
+        dropping_v_xcor(i, j, :) = cor / length(neigh{i, mastertime, masterlayer, j});
+    end
+    
+end
+
+%% cross corr of neucleus velocity
+dropping_v_xcor_avg = NaN(norder, size(dropping_v_xcor, 3));
+for i = 1:size(dropping_v_xcor, 3)
+    dropping_v_xcor_avg(:, i) = my_mean(dropping_v_xcor(:,:,i)');
+end
+figure; plot(-tlength+1+1:tlength-1-1, dropping_v_xcor_avg');
+legend('1','2','3','4','5');
+xlabel('delay')
+ylabel('normalized cross correlation')
+title('cross correlation of nucleus velocity over time for different neighbor orders')
+
+
 
 
 
--- Meta Measurements/numneighbors.m
+++ Meta Measurements/numneighbors.m
@@ -14,18 +14,44 @@ layers_from_top = 3;
     extract_measurement(data_set, measurement, cell_inds, layers_from_top);
 
 %% take distribution over cells
-out = [];
-%% 
-out(:,1)=neighbor_results(data_apical, 'apical');
-%%
-out(:,2)=neighbor_results(data_basal,  'basal');
-%%
-out(:,3)=neighbor_results(data_middle, 'middle');
+% out = [];
+% %% 
+% out(:,1)=neighbor_results(data_apical, 'apical');
+% %%
+% out(:,2)=neighbor_results(data_basal,  'basal');
+% %%
+% out(:,3)=neighbor_results(data_middle, 'middle');
 
 
 %% matthias wants the image thing. OK
 plot_neighbor_image(diff(data_apical), 5);
+title('nneighbor diff vs. time, apical');
 %%
 plot_neighbor_image(data_middle, 16);
+title('nneighbor diff vs. time, middle');
 %%
 plot_neighbor_image(diff(data_basal),  7);
+title('nneighbor diff vs. time, basal');
+
+%% 
+plot_neighbor_image(diff(squeeze(data_all(1, :, :))), 101)
+title('nneighbor diff vs z, time 0');
+%%
+
+% for cells that have a transition in z, plot the transition location vs
+% time
+
+data2 = data_all(:, 1:end-3,:); % get rid of junk at the top
+
+% my_sum(diff(squeeze(data_all(1, :, :)))')
+%%
+c=158;  %158 102 141 161
+d = diff(data2(:,:,c)');
+[z1 t1] = find(d < 0);
+[z2 t2] = find(d > 0);
+% z(6)=[]; t(6) =[];
+figure; plot(t1, z1, '.b'); hold on; plot(t2, z2, '.r');
+legend('apical->basal vertex gains', 'apical-> basal vertex losses')
+% vertex-exchange seems to move downwards... and all seem to be RED
+% red means d > 0, means you GAIN a vertex moving up, or lose one as you
+% move basally
