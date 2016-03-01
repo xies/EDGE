8b94d7b70be28111c0c5d7fc1343d547ee264e04
xies <xies@mit.edu>
2016-03-01 13:41:39 -0500

    added reloading from cached intermediates

--- Matlab/get_membs_disperse.m
+++ Matlab/get_membs_disperse.m
@@ -1,7 +1,6 @@
 function [mask,status_mse,status_skel] = get_membs_disperse(filename, cutoff, X,Y)
 
 
-cutoff = 10;
 
 [file_dir,name,ext] = fileparts( filename );
 im = imread(filename);
@@ -11,28 +10,35 @@ imwrite(im,filename);
 Nsmooth = 3;
 % Calculate Morse Smale complex, cut at cutoff, and convert the filament structure:
 
-mse_cmds = strjoin({ ...
-    'mse' , filename, ...
-    '-outDir', file_dir, ...
-    '-cut', num2str(cutoff), ...
-    ' -dumpArcs CUD'} , ' ');
-
-[status_mse,result] = system(mse_cmds);
+% Check if reload
+files = dir(file_dir);
+skl_filename = [filename '_c' num2str(cutoff) '.CUD.NDskl'];
+if ~any( cellfun(@(x) strcmpi(x,skl_filename),{files.name}) )
+    
+    mse_cmds = strjoin({ ...
+        'mse' , filename, ...
+        '-outDir', file_dir, ...
+        '-cut', num2str(cutoff), ...
+        ' -dumpArcs CUD'} , ' ');
+    
+    [status_mse,result] = system(mse_cmds);
+end
 
 %
-skl_filename = [filename '_c' num2str(cutoff) '.CUD.NDskl'];
-
-skl_cmds = strjoin({ ...
-    'skelconv', skl_filename, ...
-    '-outDir', file_dir, ...
-    '-smooth', num2str(Nsmooth), ...
-    '-to', 'NDskl_ascii'}, ' ');
-
-[status_skel,result] = system(skl_cmds);
-
+files = dir(file_dir);
 output_filename = [filename ...
     '_c' num2str(cutoff) ...
     '.CUD.NDskl.S003.a.NDskl'];
+if ~any( cellfun(@(x) strcmpi(x,output_filename),{files.name}) )
+    
+    skl_cmds = strjoin({ ...
+        'skelconv', skl_filename, ...
+        '-outDir', file_dir, ...
+        '-smooth', num2str(Nsmooth), ...
+        '-to', 'NDskl_ascii'}, ' ');
+    
+    [status_skel,result] = system(skl_cmds);
+end
 
 MSC = parseNDskl(output_filename);
 
